f356778e0fca5d5fbf02d31521dc3603a32f8cff
Denys Belinskyi <denisbelinskiy94@gmail.com>
2026-08-24 17:33:34 +0100

    bugfixing

--- js/task-3.js
+++ js/task-3.js
@@ -5,6 +5,6 @@ function getElementWidth(content, padding, border) {
   return contentValue + paddingValue * 2 + borderValue * 2;
 }
 
-console.log(getElementWidth('50px', '8px', '4px')); // 74
-console.log(getElementWidth('60px', '12px', '8.5px')); // 101
-console.log(getElementWidth('200px', '0px', '0px')); // 200
+console.log(getElementWidth(50, 8, 4)); // 74
+console.log(getElementWidth(60, 12, 8.5)); // 101
+console.log(getElementWidth(200, 0, 0)); // 200
