bb86620395b5fb7c3c34c7f77e3586e646196b1a
Denys Belinskyi <denisbelinskiy94@gmail.com>
2026-08-24 17:26:56 +0100

    bugfixing

--- js/task-3.js
+++ js/task-3.js
@@ -1,7 +1,7 @@
 function getElementWidth(content, padding, border) {
-  const contentValue = parseFloat(content);
-  const paddingValue = parseFloat(padding);
-  const borderValue = parseFloat(border);
+  const contentValue = Number.parseFloat(content);
+  const paddingValue = Number.parseFloat(padding);
+  const borderValue = Number.parseFloat(border);
   return contentValue + paddingValue * 2 + borderValue * 2;
 }
 
